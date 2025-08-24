446ce5c753f8891e5fe77ff354ee2ffc27ffbe1d
stlawton <lawtons347@gmail.com>
2025-08-24 16:58:51 -0400

    Completed code, passed tests

--- src/times_ten.py
+++ src/times_ten.py
@@ -1,1 +1,7 @@
-# Write your solution here
+def times_ten(start_index: int, end_index: int):
+    my_dictionary = {}
+    number = start_index
+    while number <= end_index:
+        my_dictionary[number] = number * 10
+        number += 1
+    return my_dictionary
